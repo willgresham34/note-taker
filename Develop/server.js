33f7b6b5484c658faca3f8e16cd917730d289ca5
Will Gresham <willgresham34@gmail.com>
2021-10-17 12:20:19 -0400

    finished delete working on post

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
-const uuid = require('uuid')
+// const uuid = require('uuid');
 
 //declares port value to deploy to heroku and set the app const to express
 const app = express();
@@ -13,10 +13,35 @@ app.use(express.json());
 app.use(express.static('public'));
 
 //Reads JSON file with notes on it
-fs.readFile('db/db.json',  (err) => err ? console.error(err) : console.log('Success!'))
+fs.readFile('db/db.json',  (err) => err ? console.error(err) : console.log(`File read`))
 
 //All paths that are needed
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 app.get('/api/notes/', (req, res) => res.sendFile(path.join(__dirname, 'db/db.json')));
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+
+app.delete('/api/notes/:id', (req, res) => {
+    const ID = req.params.id
+    fs.readFile('db/db.json', (err, data) => {
+        if(err) {
+            console.error(err)
+        }
+        const notesJSON = JSON.parse(data) 
+        const filterNotes = notesJSON.filter(function(idCheck){
+            return idCheck !== ID
+        })
+        fs.writeFile('db/db.json', JSON.stringify(filterNotes), (err) => err ? console.error(err) : console.log(`Deleted`))  
+    })
+})
+
+app.post('/api/notes', (req, res) => {
+    
+})
+
+
+
+// Shows where website is listed at and gives local link
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`));
